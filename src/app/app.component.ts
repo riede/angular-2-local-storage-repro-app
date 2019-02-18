03e764f231a06a1ff5ca878fc172b1c80710ae2d
Peter Riedemann <p.riedemann@salt-and-pepper.eu>
2019-02-18 13:06:43 +0100

    add angular-2-local-storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LocalStorageService } from 'angular-2-local-storage';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'repro-app';
+
+  constructor(private localStorageService: LocalStorageService) {
+    this.localStorageService.set('testValue', 'test');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LocalStorageModule } from 'angular-2-local-storage';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    LocalStorageModule.forRoot({
+      prefix: 'test',
+      storageType: 'localStorage'
+    }),
     AppRoutingModule
   ],
   providers: [],
